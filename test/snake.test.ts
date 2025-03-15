654291e4f1f00b4205b38f7d40f8d40638aeb469
Marcos Rangel <mrangel@gmail.com>
2025-03-15 12:56:35 -0300

    change for toStrictEqual comparasion

--- test/snake.test.ts
+++ test/snake.test.ts
@@ -6,7 +6,7 @@ describe("Snake function", () => {
 
     const result = Snake(testMatrix);
 
-    expect(result).toEqual([]);
+    expect(result).toStrictEqual([]);
   });
   it("should return an array of matrix 3x3", () => {
     const testMatrix: InputSnake = [
@@ -17,7 +17,7 @@ describe("Snake function", () => {
 
     const result = Snake(testMatrix);
 
-    expect(result).toEqual([1, 2, 3, 6, 9, 8, 7, 4, 5]);
+    expect(result).toStrictEqual([1, 2, 3, 6, 9, 8, 7, 4, 5]);
   });
   it("should return an array of matrix 4x4", () => {
     const testMatrix: InputSnake = [
@@ -29,7 +29,7 @@ describe("Snake function", () => {
 
     const result = Snake(testMatrix);
 
-    expect(result).toEqual([
+    expect(result).toStrictEqual([
       1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10,
     ]);
   });
@@ -44,7 +44,7 @@ describe("Snake function", () => {
 
     const result = Snake(testMatrix);
 
-    expect(result).toEqual([
+    expect(result).toStrictEqual([
       1, 2, 3, 4, 5, 10, 15, 20, 25, 24, 23, 22, 21, 16, 11, 6, 7, 8, 9, 14, 19,
       18, 17, 12, 13,
     ]);
